475a24793bc31232cbd4ed89d670de4e2e9b16e0
SherazDeveloper12 <talhayounus065@gmail.com>
2025-12-26 23:47:22 +0500

    Add outdoor showcase component and images
    - Introduced HomeOutDoorShowcase component to display outdoor furniture.
    - Added images for outdoor chairs and background.
    - Implemented a grid layout with various colored sections for visual appeal.
    - Styled component with Tailwind CSS for responsive design.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HomeHero from '../components/HomeHero/HomeHero'
 import DealsOffer from '../components/DealsOffer/DealsOffer'
+import HomeOutDoorShowcase from '../components/HomeOutDoorShowcase/HomeOutDoorShowcase'
 export default function Home() {
   return (
     <div className='flex flex-col gap-5 py-5'>
       <HomeHero />
       <DealsOffer />
+      <HomeOutDoorShowcase />
     </div>
   )
 }
