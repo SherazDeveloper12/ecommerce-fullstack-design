f84bcbfebae80b1a5a3b16c82f1b32760bf6c3ee
SherazDeveloper12 <talhayounus065@gmail.com>
2025-12-28 21:51:30 +0500

    feat: Enhance Products page with dropdown filters and star rating component

---
+++ src/components/DropDown/DropDown.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { ChevronRight, Star } from 'lucide-react'
+import Stars from '../Stars/Stars';
+export default function DropDownRadio(props) {
+    const [isOpen, setIsOpen] = React.useState(true);
+    const Rating = [5, 4, 3, 2, 1];
+    return (
+        <div >
+            <div className='flex justify-between cursor-pointer items-center border-t border-gray-300 py-2 bg-[#F7FAFC] overflow-hidden z-40 ' onClick={() => setIsOpen(!isOpen)}>
+                <h1 className=' font-bold text-xl'>{props.heading}</h1>
+                <span className={isOpen ? 'rotate-90 transition-transform duration-300 ease-in-out' : ''}>
+                    <ChevronRight color='gray' />
+                </span>
+            </div >
+            {
+                props.Links &&
+                <div className={isOpen ? ' flex flex-col py-2 gap-2 text-gray-500 font-semibold ' : 'hidden'}>
+                    {props.Links.map((link, index) => (
+                        <a key={index} href={link.route} className=" hover:bg-gray-100">
+                            {link.name}
+                        </a>
+                    ))}
+                </div>
+            }
+            {
+                props.RadioName &&
+                <div className={isOpen ? ' flex flex-col py-2 gap-2 text-gray-500 font-semibold ' : 'hidden'}>
+                    {props.RadioName.map((name, index) => (
+                        <div key={index} className='flex items-center gap-2'>
+                            <input type='radio' id={name} name={props.heading} value={name} defaultChecked={name === 'Any'} onChange={props.onChange} className='cursor-pointer' />
+                            <label htmlFor={name} className='cursor-pointer'>{name}</label>
+                        </div>
+                    ))}
+                </div>
+            }
+            {
+                props.PriceRange &&
+                <div className={isOpen ? ' flex flex-col py-2 gap-2 text-gray-500 font-semibold ' : 'hidden'}>
+                    <div className='flex items-center gap-2'>
+                        <label htmlFor='min' className='cursor-pointer'>Min:</label>
+                        <input type='number' id='min' name='min' defaultValue={props.PriceRange[0]} className='border border-gray-300 rounded-md p-1 w-20' onChange={props.onChange} />
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <label htmlFor='max' className='cursor-pointer'>Max:</label>
+                        <input type='number' id='max' name='max' defaultValue={props.PriceRange[1]} className='border border-gray-300 rounded-md p-1 w-20' onChange={props.onChange} />
+                    </div>                  
+                    
+                </div>
+            }
+            {
+                props.CheckboxName &&
+                <div className={isOpen ? ' flex flex-col py-2 gap-2 text-gray-500 font-semibold ' : 'hidden'}>
+                    {props.CheckboxName.map((name, index) => (
+                        <div key={index} className='flex items-center gap-2'>
+                            <input type='Checkbox' id={name} name={props.heading} value={name} onChange={props.onChange} className='cursor-pointer' />
+                            <label htmlFor={name} className='cursor-pointer'>{name}</label>
+                        </div>
+                    ))}
+                </div>
+            }
+            {
+                props.Rating &&
+                <div className={isOpen ? ' flex flex-col py-2 gap-2 text-gray-500 font-semibold ' : 'hidden'}>
+
+                    {Rating.map((rate, index) => (
+
+                        <div className='flex items-center gap-2'>
+                            <input type='Checkbox' id={`${rate}-Stars`} value={`${rate}-Stars`} className='cursor-pointer' />
+                            <label htmlFor={`${rate}-Stars`} className='cursor-pointer '>
+                                <Stars rating={rate} />
+                            </label>
+                        </div >
+
+                    ))}
+
+
+                </div>
+            }
+        </div >
+    )
+}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,62 +1,83 @@
 import React from 'react'
 import mobile from '../assets/11.png'
-import { Star, Circle, Heart } from 'lucide-react'
+import { Star, Circle, Heart, ChevronRight } from 'lucide-react'
 import { products } from '../lib/constant'
+import DropDown from '../components/DropDown/DropDown'
+import DropDownRadio from '../components/DropDown/DropDown'
+import NewsLetterSubscription from '../components/NewsLetterSubscription/NewsLetterSubscription'
+import Stars from '../components/Stars/Stars'
 export default function Products() {
+    const links = [
+        {
+            name: 'Mobile accessory',
+            route: '/mobile-accessory'
+        },
+        {
+            name: 'Electronics',
+            route: '/electronics'
+        },
+        {
+            name: 'Smartphones ',
+            route: '/smartphones'
+        },
+        {
+            name: 'Modern tech',
+            route: '/modern-tech'
+        },
+    ]
 
     return (
-        <div className='flex max-w-7xl mx-auto gap-4 pt-4'>
-            <div className='bg-blue-400 flex-1'>Filters</div>
-            <div className='flex-4 flex flex-col gap-4 overflow-y-auto '>
-                {products.map((product, index) => (
-                    <div className='flex gap-4 bg-white rounded-lg border border-gray-300 shadow-md' id={index}>
-                    <div className='flex-1 flex justify-center items-center'>
-                        <img src={product.img} alt={product.title} />
-                    </div>
-                    <div className='flex-3 flex flex-col gap-3 p-4 relative'>
-                        <h1>{product.heading}</h1>
-                        <div>
-                        <div className='flex gap-4'>
-                            <p className='font-semibold'>${product.price}.00</p>
-                            <p className='text-gray-500 line-through'>$1128.00</p>
-                        </div>
-                        <div className='flex gap-2'>
-                            <span className='flex '>
-                               {Array.from({ length: product.rating }).map((_, i) => (
-                                    <Star
-                                        key={i}
-                                      fill='orange'
-                                        stroke='0'
-                                    />
-                                ))}
-                                {Array.from({ length: 5 - product.rating }).map((_, i) => (
-                                    <Star
-                                        key={i}
-                                      fill='#dee2e7'
-                                        stroke='0'
-                                    />
-                                ))}
-                            </span>
-                            <span className='text-[#dee2e7] flex gap-2 justify-center items-center'>
-                                <Circle fill='#dee2e7' stroke='0' size={10} />
-                                <p className='font-semibold text-gray-400'>{product.orders} Orders</p>
-                                <Circle fill='#dee2e7' stroke='0' size={10} />
-                            </span>
-                            {product.freeShipping ? <p className='font-semibold text-green-400'>Free Shipping</p> : ''}
-                        </div>
-                        </div>
-                        <div>
-                            <p className='text-gray-400 pr-32'>{product.description}</p>
-                        </div>
-                        <a href="#" className='text-blue-400 font-semibold'>View Details</a>
-                        <div className='flex justify-center items-center rounded-lg border border-blue-600 p-2 absolute top-4 right-4 cursor-pointer'>
-                            <Heart color='blue' />
-                        </div>
-                    </div>
+        <div className=''>
+            <div className='flex max-w-7xl mx-auto gap-4 py-4'>
+                <div className='flex-1 flex flex-col gap-4 '>
+                    <DropDown Links={links} heading='All Categories' />
+
+                    <DropDown CheckboxName={['Samsung', 'Huawei', 'Apple', 'Xiaomi', 'Lenovo']} heading='Brands' />
+                    <DropDown CheckboxName={['Metallic', 'Plastic', 'Leather', 'Wood', 'Glass']} heading='Features' />
+                    <DropDown PriceRange={[0, 20000]} heading='Price Range' />
+                    <DropDown RadioName={['Any', 'Refurbished', 'New', 'Used']} heading='Condition' />
+                    <DropDown Rating={true} heading='Rating' />
                 </div>
-                ))}
-                
+                <div className='flex-4 flex flex-col gap-4 overflow-y-auto '>
+                    {products.map((product, index) => (
+                        <div className='flex gap-4 bg-white rounded-lg border border-gray-300 shadow-md' id={index}>
+                            <div className='flex-1 flex justify-center items-center'>
+                                <img src={product.img} alt={product.title} />
+                            </div>
+                            <div className='flex-3 flex flex-col gap-3 p-4 relative'>
+                                <h1>{product.heading}</h1>
+                                <div>
+                                    <div className='flex gap-4'>
+                                        <p className='font-semibold'>${product.price}.00</p>
+                                        <p className='text-gray-500 line-through'>$1128.00</p>
+                                    </div>
+                                    <div className='flex gap-2'>
+                                        <Stars rating={product.rating} />
+                                        <span className='text-[#dee2e7] flex gap-2 justify-center items-center'>
+                                            <Circle fill='#dee2e7' stroke='0' size={10} />
+                                            <p className='font-semibold text-gray-400'>{product.orders} Orders</p>
+                                            <Circle fill='#dee2e7' stroke='0' size={10} />
+                                        </span>
+                                        {product.freeShipping ? <p className='font-semibold text-green-400'>Free Shipping</p> : ''}
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className='text-gray-400 pr-32'>{product.description}</p>
+                                </div>
+                                <a href="#" className='text-blue-400 font-semibold'>View Details</a>
+                                <div className='flex justify-center items-center rounded-lg border border-blue-600 p-2 absolute top-4 right-4 cursor-pointer'>
+                                    <Heart color='blue' />
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+
+                </div>
             </div>
+            <NewsLetterSubscription />
         </div>
     )
 }
+
+
+
